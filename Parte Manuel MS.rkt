927627111344659bae9aeccc70d177c648d2b630
Manuel MS <darkside16@gmail.com>
2018-10-27 14:37:27 -0600

    Update Parte Manuel MS.rkt

--- Parte Manuel MS.rkt
+++ Parte Manuel MS.rkt
@@ -49,44 +49,49 @@
                      init
                      (rest lst)))]))
 
-; **************************************************************
+; *****************************Evaluacion de la funcion*********************************
 
 (evalPol 3 '(-19 7 -4 6))
 (horner 3 '(-19 7 -4 6))
 
 ;*******************agregado recientemente******************
 
-%Division de polinomios
-#lang racket
-(define (deg p) 
+; Implementacion de la funcion para dividir polinomios
+; Esta funcion genera una lista con los valores del residuo y otra lista con los valores del cociente
+
+(define (grado p) 
   (for/fold ([d -inf.0]) ([(pi i) (in-indexed p)])
     (if (zero? pi) d i)))
-(define (lead p) (vector-ref p (deg p)))
+(define (lead p) (vector-ref p (grado p)))
 (define (mono c d) (build-vector (+ d 1) (λ(i) (if (= i d) c 0))))
-(define (poly*cx^n c n p) (vector-append (make-vector n 0) (for/vector ([pi p]) (* c pi))))
-(define (poly+ p q) (poly/lin 1 p  1 q))
-(define (poly- p q) (poly/lin 1 p -1 q))
-(define (poly/lin a p b q)
-  (cond [(< (deg p) 0) q] 
-        [(< (deg q) 0) p]
-        [(< (deg p) (deg q)) (poly/lin b q a p)]
-        [else (define ap+bq (for/vector #:length (+ (deg p) 1) #:fill 0
+(define (divPol*cx^n c n p) (vector-append (make-vector n 0) (for/vector ([pi p]) (* c pi))))
+(define (divPol+ p q) (divPol/lin 1 p  1 q))
+(define (divPol- p q) (divPol/lin 1 p -1 q))
+(define (divPol/lin a p b q)
+  (cond [(< (grado p) 0) q] 
+        [(< (grado q) 0) p]
+        [(< (grado p) (grado q)) (divPol/lin b q a p)]
+        [else (define ap+bq (for/vector #:length (+ (grado p) 1) #:fill 0
                               ([pi p] [qi q]) (+ (* a pi) (* b qi))))
-              (for ([i (in-range (+ (deg q) 1) (+ (deg p) 1))])
+              (for ([i (in-range (+ (grado q) 1) (+ (grado p) 1))])
                 (vector-set! ap+bq i (* a (vector-ref p i))))
               ap+bq]))
  
-(define (poly/ n d)
-  (define N (deg n))
-  (define D (deg d))
+(define (divPol/ n d)
+  (define N (grado n))
+  (define D (grado d))
   (cond
-    [(< N 0) (error 'poly/ "can't divide by zero")]
+    [(< N 0) (error 'divPol/ "No puede dividir entre 0")]
     [(< N D) (values 0 n)]
     [else    (define c (/ (lead n) (lead d)))
              (define q (mono c (- N D)))
-             (define r (poly- n (poly*cx^n c (- N D) d)))
-             (define-values (q1 r1) (poly/ r d))
-             (values (poly+ q q1) r1)]))
+             (define r (divPol- n (divPol*cx^n c (- N D) d)))
+             (define-values (q1 r1) (divPol/ r d))
+             (values (divPol+ q q1) r1)]))
+
+; *****************************Evaluacion de la funcion*********************************
+
+(divPol/ #(-42 0 -12 1) #(-3 1))
 
 ;*******************agregado recientemente******************
 

97dc7b6cb7ea47448bdae4b32a03fde34db54c4a
ManuelCesp <manuel.cespedes.osorio@est.una.ac.cr>
2018-10-28 18:07:43 -0600

    Multiplicaci[on lista

--- Parte Manuel Cesp.rkt
+++ Parte Manuel Cesp.rkt
@@ -42,7 +42,110 @@
     )
   )
 
+(define printL
+    (lambda (L i)
+        (
+            cond(
+                (< i (length L))
+                (string-append(printE (list-ref L i) i)(printL L (+ i 1))))
+                (else "")
+        )
+    )
+)
+
+(define despliegue
+    (lambda (L)
+        (display(printL L 0))
+    )
+)
+
+;Meodo para ayudar sumas y restas
+; ([1,2],[1,4,5]=> [1,2,0],[1,4,5])
+(define emparejar
+  (lambda (L1 L2)
+    ( cond( (< (length L1)(length L2))
+          (emparejar(append L1 '(0)) L2)
+      )(else L1)
+    )
+  )
+)
+
+(define agregaCerosInicio
+  (lambda (L L0)
+    (cond ((null? (cdr L)) (list(append L0 (car L))))
+          (else
+           (cons (append L0 (car L)) (agregaCerosInicio (cdr L) (cons 0 L0)))))))
+
+;Método para sumar
+;[1 2 3] + [1 2 3] = [2 4 6]
+;[1 2 3] + [1 2] => (1 + 2x + 3x^2) + (1 + 2x + 0x^2)
+;[1 2 3] + [1 0 3]
+;(1 2x 3x^2) + (1 + 0x +3x^2)
+(define sumar
+  (lambda (L1 L2 i)
+    (cond((< i (length L1))
+          (cons ( + (list-ref L1 i) (list-ref L2 i)) (sumar L1 L2 (+ i 1)))   
+     ) (else '())
+    )
+  )
+)
+
+
+(define sumapolinomios
+  (lambda (L1);recibe una lista de listas
+    (cond ( (> (length L1) 2)
+          (sumapolinomios (cons(sumapolinomios(cons(car L1) (cons (car(cdr L1))'()) )) (cddr L1)));THEN
+      )(else (
+              simplificar(
+                  sumar (emparejar(car L1)(list-ref L1 1)) (emparejar(list-ref L1 1)(car L1)) 0
+              )
+             )
+        )
+    )
+  )
+)
+
+;MULTIPLICAR
+;multiplica los elem de una lista x un Elemento
+(define multiplicaListElem
+ (lambda (L E)
+   (cond ((null? (cdr L)) (list (* E (car L))))
+         (else
+          (cons (* (car L) E) (multiplicaListElem (cdr L) E))))))
+
+;multiplica 2 listas
+(define multiplicar
+  (lambda (L1 L2)
+    (cond ((null? (cdr L2)) (list (multiplicaListElem L1 (car L2))))
+          (else
+           (cons (multiplicaListElem L1 (car L2)) (multiplicar L1 (cdr L2)))))))
+
+;multiplica las sublistas de una lista de listas x los elementos de una lista de enteros
+(define multListsPorList
+  (lambda (LL L)
+    (cond ((null? (cdr LL)) (agregaCerosInicio(multiplicar (car LL) L) '()))
+          (else
+           (append (agregaCerosInicio(multiplicar (car LL) L) '()) (multListsPorList (cdr LL) L))))))
+
+;multiplica mas de 2 listas
+(define fx
+  (lambda (L1 L2)
+    (cond ((null? (cdr L2)) (multListsPorList L1 (car L2)))
+          (else
+           (fx (multListsPorList L1 (car L2)) (cdr L2))))))
+
+(define multiplicaPolinomios
+  (lambda (LL)
+    (cond ((> (length LL) 2) (fx (agregaCerosInicio (multiplicar (car LL) (cadr LL))'()) (cddr LL)))
+          (else
+           (agregaCerosInicio (multiplicar (car LL)(cadr LL))'())))))
+     
+(define *p
+  (lambda (L)
+    (despliegue (sumapolinomios (multiplicaPolinomios L)))))
 ;****************************PRUEBAS***************************
 ;(remove-last L)
 ;(simplificar L)
 ;(printE 0 9) || (printE 2 5) || (printE 8 0)
+;(*p '((3 0 2)(4 0 3 2)))
+;(*p '((-2 3)(-11 4 2)))
